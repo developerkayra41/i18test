1d3680250dda3d63b5551c8a58df85fcdf913893
Kayra Özgür <kayra.sagopa23@hotmail.com>
2024-11-21 17:38:02 +0300

    .

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -2,6 +2,8 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 
+if (!localStorage.getItem('language')) localStorage.setItem('language', 'tr')
+
 i18n
     .use(initReactI18next)
     .use(Backend)
